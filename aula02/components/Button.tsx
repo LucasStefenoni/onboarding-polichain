9f11d9575db2b877351345bc66563ff8f04dafa4
LucasStefenoni <lucasstefenonir@gmail.com>
2025-06-09 18:22:28 -0300

    consertando home page

--- aula02/components/Button.tsx
+++ aula02/components/Button.tsx
@@ -9,7 +9,7 @@ type ButtonProps = {
 export default function Button({texto}: ButtonProps) {
   return (
     <div>   
-        <Link href="/Home" className="text-white bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded">
+        <Link href="/" className="text-white bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded">
             {texto}
         </Link>
     </div>
