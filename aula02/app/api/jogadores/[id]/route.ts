3408d592d90b82bbaf23aa78b6f2e30055e7b758
LucasStefenoni <lucasstefenonir@gmail.com>
2025-06-26 12:46:04 -0300

    consertando [id]

--- aula02/app/api/jogadores/[id]/route.ts
+++ aula02/app/api/jogadores/[id]/route.ts
@@ -1,15 +1,15 @@
 import {NextResponse} from 'next/server'
 import {prisma} from '@/lib/prisma'
 
-export async function PUT(request:Request, {params}: {params: {id:string}}) {
-    const id = await Number(params.id)
+export async function PUT(request:Request, {params}: {params: Promise<{id:string}>}) {
+    const id = Number((await params).id)
     const [pontos] = await request.json()
     const updated = await prisma.jogador.update({where: {id}, data: {pontos}})
     return NextResponse.json(updated)
 }
 
-export async function DELETE(request:Request, {params}: {params: {id:string}}) {
-    const id = Number(params.id)
+export async function DELETE(request:Request, {params}: {params: Promise<{id:string}>}) {
+    const id = Number((await params).id)
     const updated = await prisma.jogador.delete({where: {id}})
     return NextResponse.json({ok: true})
 }
